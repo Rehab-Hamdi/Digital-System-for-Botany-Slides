b20b2393d603ac4d9a5a69b3bdbfa0bcc4fbfd75
rehab <rehab123selim@gmail.com>
2023-02-15 16:04:08 +0200

    colors

---
+++ lib/core/colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MyColors {
+  static const Color bink = Color(0xffad4363);
+  static const Color lightPink = Color(0xff7a1a36);
+  static const Color gray = Color(0xffc8cccc);
+  static const Color lightBlack = Color(0xff868181);
+  static const Color offwhite = Color(0xfffbfdf8);
+  static const Color green = Color(0xf5193E46);
+  static const Color lightGray = Color(0xf3f3f3f3);
+}
