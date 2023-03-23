580fa12dad9d7f53772a6137f299225c2c1ee679
rehab <rehab123selim@gmail.com>
2023-03-24 00:31:18 +0200

    category model

---
+++ lib/models/categorg_model.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CategoryModel
+{
+  final Color color;
+  final String categoryNumber;
+  final String categoryName;
+  CategoryModel({
+    required this.color,
+    required this.categoryNumber,
+    required this.categoryName,
+  });
+
+}
