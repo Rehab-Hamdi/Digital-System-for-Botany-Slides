8351f7542c3423be8fa20ff8f979baa9a8cfb089
rehab <rehab123selim@gmail.com>
2023-03-24 00:28:20 +0200

    change home view

--- lib/presentation/user_application_screens/home/home_view.dart
+++ lib/presentation/user_application_screens/home/home_view.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:our_test_project/core/styles/colors.dart';
 import 'package:our_test_project/presentation/user_application_screens/calendar/calendar_view.dart';
+import 'package:our_test_project/presentation/user_application_screens/categories/categories_view.dart';
 import 'package:our_test_project/presentation/user_application_screens/favorites/favorites_veiw.dart';
 import 'package:our_test_project/presentation/user_application_screens/home/mobile_home_view.dart';
 import 'package:our_test_project/presentation/user_application_screens/settings/seetings_view.dart';
@@ -21,7 +22,7 @@ class HomeScreen extends StatefulWidget
 class _HomeView extends State<HomeScreen> {
 
   List<Widget> pages = [
-    ResponsiveLayout(mobileWidget: MobileHomeView()),
+    ResponsiveLayout(mobileWidget: CategoryView()),
     const FavoriteView(),
     const CalendarView(),
     const SettingsView(),
--- lib/presentation/user_application_screens/home/mobile_home_view.dart
+++ lib/presentation/user_application_screens/home/mobile_home_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:our_test_project/core/custom_widgets/plants_card_item.dart';
 import 'package:our_test_project/core/styles/colors.dart';
 import 'package:our_test_project/models/plants_models.dart';
+import 'package:our_test_project/presentation/user_application_screens/home/home_view.dart';
 
 class MobileHomeView extends StatefulWidget {
   static const String routeName = "MobileHomeView";
@@ -75,130 +76,123 @@ class _MobileHomeViewState extends State<MobileHomeView> {
   }
 
   Widget build(BuildContext context) {
-
     return Scaffold(
-      body: SafeArea(
-        child: SizedBox(
+        body: SafeArea(
           child: SingleChildScrollView(
-            child: Center(
               child: Column(
-                children: [
-                  Container(
-                    margin: const EdgeInsets.only(top: 15, left: 15, right: 15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.all(7.0),
-                          child: const Text( "Let's Find \nYour Slide",
-                            style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),) ,
-                        ),
-
-                        ///New Code for Search
-                        Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(40.0),
-                              border: Border.all(
-                                color: MyColors.lightGreen,
-
-                              )
-
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(top: 15, left: 15, right: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            padding: const EdgeInsets.all(7.0),
+                            child: const Text( "Let's Find \nYour Slide",
+                              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),) ,
                           ),
-                          child: Padding(
-                            padding: const EdgeInsets.only(right: 5, left: 5),
-                            child: TextField(
-                              onChanged: (value) {
-                                setState(() {
-                                  itemsListSearch = PlantsName
-                                      .where((element) => element
-                                      .toLowerCase()
-                                      .contains(value.toLowerCase()))
-                                      .toList();
-                                  if (_searchTextController!.text.isNotEmpty &&
-                                      itemsListSearch!.isEmpty) {
-                                    print(
-                                        'itemsListSearch legnth${itemsListSearch!.length}');
-                                  }
-                                });
-                              },
-                              controller: _searchTextController,
-                              focusNode: _node,
-                              decoration: InputDecoration(
-                                  hintText: 'Find_Your_Slide ',
-                                  hintStyle:
-                                  Theme.of(context).textTheme.bodySmall,
-                                  border: InputBorder.none,
-                                  prefixIcon: const Icon(
-                                    Icons.search,
-                                    color: Colors.grey,
-                                  ),
-                                  suffixIcon: IconButton(
-                                    onPressed:
-                                    _searchTextController!.text.isEmpty
-                                        ? null
-                                        : () {
-                                      _searchTextController?.clear();
-                                      _node.unfocus();
-                                    },
-                                    icon: Icon(
-                                      Icons.cancel,
-                                      color:
-                                      _searchTextController!.text.isNotEmpty
-                                          ? Colors.red
-                                          : Colors.grey,
+
+                          ///New Code for Search
+                          Container(
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(40.0),
+                                border: Border.all(
+                                  color: MyColors.lightGreen,
+
+                                )
+
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.only(right: 5, left: 5),
+                              child: TextField(
+                                onChanged: (value) {
+                                  setState(() {
+                                    itemsListSearch = PlantsName
+                                        .where((element) => element
+                                        .toLowerCase()
+                                        .contains(value.toLowerCase()))
+                                        .toList();
+                                    if (_searchTextController!.text.isNotEmpty &&
+                                        itemsListSearch!.isEmpty) {
+                                      print(
+                                          'itemsListSearch legnth${itemsListSearch!.length}');
+                                    }
+                                  });
+                                },
+                                controller: _searchTextController,
+                                focusNode: _node,
+                                decoration: InputDecoration(
+                                    hintText: 'Find_Your_Slide ',
+                                    hintStyle:
+                                    Theme.of(context).textTheme.bodySmall,
+                                    border: InputBorder.none,
+                                    prefixIcon: const Icon(
+                                      Icons.search,
+                                      color: Colors.grey,
                                     ),
-                                  )),
+                                    suffixIcon: IconButton(
+                                      onPressed:
+                                      _searchTextController!.text.isEmpty
+                                          ? null
+                                          : () {
+                                        _searchTextController?.clear();
+                                        _node.unfocus();
+                                      },
+                                      icon: Icon(
+                                        Icons.cancel,
+                                        color:
+                                        _searchTextController!.text.isNotEmpty
+                                            ? Colors.red
+                                            : Colors.grey,
+                                      ),
+                                    )),
+                              ),
                             ),
                           ),
-                        ),
-                        _searchTextController!.text.isNotEmpty && itemsListSearch!.isEmpty
-                            ? Column(
-                          children: [
-                            SizedBox(height: MediaQuery.of(context).size.height*0.03,),
-                            Container(
-                              height: MediaQuery.of(context).size.height*0.65,
-                              decoration: const BoxDecoration(
-                                  image: DecorationImage(
-                                    image:AssetImage('assets/images/noResult.jpg',),
-                                    fit: BoxFit.cover,
-                                  )
-                              ),
-                            )
-                          ],
-                        )
-
-                            :
-                        /// plants Show
-                        SizedBox(
-                          height:
-                          MediaQuery.of(context).size.height * 0.70,
-                          child: GridView.builder(
-                              gridDelegate:
-                              const SliverGridDelegateWithFixedCrossAxisCount(
-                                crossAxisCount: 2,
-                                crossAxisSpacing: 1,
-                                mainAxisSpacing: 4,
-                                childAspectRatio: 1,
-                              ),
-                              itemCount:_searchTextController.text.isEmpty? PlantsName.length: itemsListSearch!.length,
-                              itemBuilder: (context, index) =>
-                              _searchTextController.text.isEmpty?
-                              PlantsCardItem(
-                                plantsModel:PlantsModel(plant_image: PlantsImage[index], plant_name: PlantsName[index]) ,
-                              ):  PlantsCardItem(
-                                plantsModel:PlantsModel(plant_image: PlantsImage[index], plant_name: itemsListSearch![index]) ,
+                          _searchTextController!.text.isNotEmpty && itemsListSearch!.isEmpty
+                              ? Column(
+                            children: [
+                              SizedBox(height: MediaQuery.of(context).size.height*0.03,),
+                              Container(
+                                height: MediaQuery.of(context).size.height*0.65,
+                                decoration: const BoxDecoration(
+                                    image: DecorationImage(
+                                      image:AssetImage('assets/images/noResult.jpg',),
+                                      fit: BoxFit.cover,
+                                    )
+                                ),
                               )
+                            ],
+                          )
+                              :
+                          /// plants Show
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height * 0.80,
+                            child: GridView.builder(
+                                gridDelegate:
+                                const SliverGridDelegateWithFixedCrossAxisCount(
+                                  crossAxisCount: 2,
+                                  crossAxisSpacing: 1,
+                                  mainAxisSpacing: 4,
+                                  childAspectRatio: 1,
+                                ),
+                                itemCount:_searchTextController.text.isEmpty? PlantsName.length: itemsListSearch!.length,
+                                itemBuilder: (context, index) =>
+                                _searchTextController.text.isEmpty?
+                                PlantsCardItem(
+                                  plantsModel:PlantsModel(plant_image: PlantsImage[index], plant_name: PlantsName[index]) ,
+                                ):  PlantsCardItem(
+                                  plantsModel:PlantsModel(plant_image: PlantsImage[index], plant_name: itemsListSearch![index]) ,
+                                )
+                            ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
-              ),
+                  ],
+                ),
             ),
-          ),
         ),
-      ),
-    );
+      );
   }
 }
