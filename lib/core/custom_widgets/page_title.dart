bb32131f0b4e3e1325fb75c93336b526f19cf5a3
rehab <rehab123selim@gmail.com>
2023-03-24 00:29:57 +0200

    chnage in custom widgets

--- lib/core/custom_widgets/page_title.dart
+++ lib/core/custom_widgets/page_title.dart
@@ -2,16 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:our_test_project/core/styles/colors.dart';
 
 class PageTitle extends StatelessWidget {
-  const PageTitle({super.key, required this.title});
+  const PageTitle({super.key, required this.title, required this.wdth});
 
   final String title;
+  final double wdth;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment: Alignment.center,
       padding: const EdgeInsets.all(10.0),
-      width: 280,
+      width: MediaQuery.of(context).size.width*wdth,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12.0),
           border: Border.all(color: MyColors.active)),
--- lib/core/custom_widgets/title_and_textField.dart
+++ lib/core/custom_widgets/title_and_textField.dart
@@ -1,56 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:our_test_project/core/styles/colors.dart';
 
-class TitleAndInputField extends StatelessWidget {
-  TitleAndInputField({
+class MobileTitleAndInputField extends StatelessWidget {
+  MobileTitleAndInputField({
     super.key,
     required this.txt,
     this.left_margin,
     required this.focusNode,
     required this.controller,
-    this.validatorFunction
+    this.validatorFunction,
+    required this.containerPadding
   });
   final String txt;
   final double? left_margin;
   FocusNode focusNode;
   var controller;
+  final double containerPadding ;
   String Function(String?)? validatorFunction;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.only(left: 60),
+    return  Padding(
+      padding: EdgeInsets.only(left: containerPadding),
       child: Row(
-        children: [
-          Container(
-            margin: EdgeInsets.only(right:left_margin!),
-            child: Text(
-              txt,
-              style: TextStyle(fontSize: 18),
-            ),
-          ),
-          SizedBox(
-            width: 350,
-            height: 40,
-            child: TextFormField(
-              controller: controller,
-              focusNode: focusNode,
-              validator: validatorFunction,
-              decoration: InputDecoration(
-                focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide: const BorderSide(
-                      color: MyColors.active,
-                    )),
-                enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(15.0),
-                    borderSide: const BorderSide(
-                      color: MyColors.active,
-                    )),
+          children: [
+            Container(
+              margin: EdgeInsets.only(right:left_margin!),
+              child: Text(
+                txt,
+                style: TextStyle(fontSize: 18),
               ),
             ),
-          ),
-        ],
-      ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width*0.60,
+              height: MediaQuery.of(context).size.height*0.05,
+              child: TextFormField(
+                controller: controller,
+                focusNode: focusNode,
+                validator: validatorFunction,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: const BorderSide(
+                        color: MyColors.active,
+                      )),
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: const BorderSide(
+                        color: MyColors.active,
+                      )),
+                ),
+              ),
+            ),
+          ],
+        ),
     );
   }
 }
