a97350a69a71f2f0bd657b07751a6593a01948ea
rehab <rehab123selim@gmail.com>
2023-04-19 03:46:21 +0200

    setting of dashboard

---
+++ lib/presentation/dashboard_application_screens/settings/desktop_settings.dart
@@ -0,0 +1,83 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:our_test_project/presentation/auth/login/login_view.dart';
+import 'package:sizer/sizer.dart';
+
+class DesktopSettingsView extends StatefulWidget {
+  static const String routeName = 'settingsView';
+
+  const DesktopSettingsView({super.key});
+
+  @override
+  State<DesktopSettingsView> createState() => _SettingsViewState();
+}
+
+class _SettingsViewState extends State<DesktopSettingsView> {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          body: ListView(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.02,
+          ),
+          ListTile(
+            title: Text(
+              'Logout',
+              style: TextStyle(fontSize: 5.sp),
+            ),
+            leading: Icon(
+              Icons.logout,
+              size: 5.sp,
+            ),
+            onTap: () async {
+              await showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: Text(
+                    'Logout',
+                    style: TextStyle(fontSize: 3.sp),
+                  ),
+                  content: Text('Are you sure you want to logout?',
+                      style: TextStyle(fontSize: 3.sp)),
+                  actions: [
+                    TextButton(
+                      child: Text('Cancel',
+                          style:
+                              TextStyle(fontSize: 4.sp, color: Colors.green)),
+                      onPressed: () => Navigator.of(context).pop(false),
+                    ),
+                    TextButton(
+                        child: Text('Logout',
+                            style:
+                                TextStyle(fontSize: 4.sp, color: Colors.red)),
+                        onPressed: () async {
+                          await FirebaseAuth.instance.signOut();
+                          //MobileLoginView.isViewed=0;
+                          Navigator.pushReplacementNamed(
+                              context, LoginView.routeName);
+                        }),
+                  ],
+                ),
+              );
+            },
+          ),
+          Divider(),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.69,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Image.asset(
+                "assets/images/plant1.png",
+              ),
+              Image.asset("assets/images/plant1.png")
+            ],
+          ),
+        ],
+      )),
+    );
+  }
+}
