14945ce4c45cc07c7a6dfbe9cbc264b72a02dde9
rehab <rehab123selim@gmail.com>
2023-05-13 23:19:13 +0300

    modify category shape
    i edit the design of categories in category screen

--- lib/core/views/categorg_card_item.dart
+++ lib/core/views/categorg_card_item.dart
@@ -13,46 +13,33 @@ class CategoryCardItem extends StatelessWidget
   Widget build(BuildContext context) {
     return  InkWell(
           onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context)=>MobileHomeView()));
+            Navigator.push(context, MaterialPageRoute(builder: (context)=>HomeView(category:categoryModel,)));
           },
           child: Padding(
             padding: const EdgeInsets.all(8.0),
-            child:Container(
-              decoration:  BoxDecoration(
-                boxShadow: [
-                  BoxShadow(
-                    color:Colors.grey.withOpacity(0.4),
-                    blurRadius: 7.0, // soften the shadow
-                    offset: const Offset(
-                      1.0, // Move to right 10  horizontally
-                      2.0, // Move to bottom 10 Vertically
-                    ),
-                  ),
-                ],
+            child:Card(
+              shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18),
               ),
-              child: Card(
-                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15),
-                ),
-                color: Colors.white,
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.50,
-                  //height: 80.sp,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
+              elevation: 5,
+              color: Colors.white,
+              child: SizedBox(
+                width: MediaQuery.of(context).size.width * 0.50,
+                //height: 80.sp,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
 
-                      CircleAvatar(
-                        backgroundColor: categoryModel.color,
-                        radius: 36,
-                        child: CircleAvatar(
-                          backgroundColor: Colors.white,
-                          radius: 35.0,
-                          child: Text(categoryModel.categoryNumber, style: TextStyle(color: categoryModel.color, fontSize: 18.sp),),
-                        ),
+                    CircleAvatar(
+                      backgroundColor: categoryModel.color,
+                      radius: 36,
+                      child: CircleAvatar(
+                        backgroundColor: Colors.white,
+                        radius: 35.0,
+                        child: Text(categoryModel.categoryNumber, style: TextStyle(color: categoryModel.color, fontSize: 18.sp),),
                       ),
-                      Text(categoryModel.categoryName, style: TextStyle(color: categoryModel.color, fontSize: 13.sp)),
-                    ],
-                  ),
+                    ),
+                    Text(categoryModel.categoryName, style: TextStyle(color: categoryModel.color, fontSize: 13.sp)),
+                  ],
                 ),
               ),
             ),
